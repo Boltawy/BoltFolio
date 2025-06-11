be4288082faad2f3caf62b7f1690529d48ca6c1a
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2025-06-11 17:27:11 +0300

    increased contrast

--- src/app/lite/P.jsx
+++ src/app/lite/P.jsx
@@ -2,7 +2,7 @@ export default function P({ children, className = "", opacity = 100 }) {
   return (
     <p
       className={
-        "pb-6 text-lg text-gray-300 " + "opacity-" + opacity + " " + className
+        "pb-6 text-lg text-gray-200 " + "opacity-" + opacity + " " + className
       }
     >
       {children}
