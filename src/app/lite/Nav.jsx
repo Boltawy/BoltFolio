77c14910976067208c3eb3d1ba1c3057bff20cd2
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2025-06-12 22:51:19 +0300

    adjusted font for nav

--- src/app/lite/Nav.jsx
+++ src/app/lite/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
 
   return (
     <ul
-      className="flex flex-col justify-center items-end fixed top-1/2 h-screen -translate-y-1/2 right-12  py-16 text-2xl gap-5 text-gray-200/50 animate__animated animate__fadeIn
+      className="flex flex-col justify-center items-end fixed top-1/2 h-screen -translate-y-1/2 right-12  py-16 text-2xl font-semibold font-inter gap-5 text-gray-200/50 animate__animated animate__fadeIn
         before:content-[''] before:w-1 before:relative before:top-0 before:right-1 before:h-1/2 before:bg-gray-200/20
       after:content-[''] after:w-1 after:relative after:bottom-0 after:right-1 after:h-1/2 after:bg-gray-200/20"
     >
