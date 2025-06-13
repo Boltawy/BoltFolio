927366d244bfa944accae0a6ff9423aaea7e8e20
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2025-06-13 13:47:51 +0300

    fixed gap in subheading

--- src/app/lite/SubHeading.jsx
+++ src/app/lite/SubHeading.jsx
@@ -1,11 +1,11 @@
 export default function SubHeading({ children, sectionTag, className }) {
   return (
-    <div className={"flex justify-between items-baseline" + " " + className}>
+    <div className={"flex justify-between gap-6 items-baseline" + " " + className}>
       <h3 className="font-semibold  text-2xl text-gray-200/60 py-4 font-sriracha tracking-wider">
         {children}
       </h3>
       <h2
-        className="text-gray-200/30 text-right -mb-[28px] text-lg pr-2 scroll-mt-6"
+        className="text-gray-200/30 text-right -mb-[28px] text-lg pr-4 scroll-mt-6"
         id={sectionTag}
       >
         {sectionTag}
